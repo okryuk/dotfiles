b0132de0847c8504c6cc7296f1b7f8990be9ff33
okryuk <okryuk@gmail.com>
2024-09-27 18:33:27 -0400

    feat: lsp.lua for better go support

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -71,14 +71,38 @@ return {
 				gopls = {
 					settings = {
 						gopls = {
-							completeUnimported = true, -- AutoImport packages when autocmplete wwas used
-							usePlaceholders = false, -- Insert placeholders for parameters in functions or methods
-							analyses = {
-								unusedparams = true, -- Show if parameters are not used
-							},
-							staticcheck = true,
-							gofumpt = true,
-						},
+						      gofumpt = true,
+						      codelenses = {
+							gc_details = false,
+							generate = true,
+							regenerate_cgo = true,
+							run_govulncheck = true,
+							test = true,
+							tidy = true,
+							upgrade_dependency = true,
+							vendor = true,
+						      },
+						      hints = {
+							assignVariableTypes = true,
+							compositeLiteralFields = true,
+							compositeLiteralTypes = true,
+							constantValues = true,
+							functionTypeParameters = true,
+							parameterNames = true,
+							rangeVariableTypes = true,
+						      },
+						      analyses = {
+							fieldalignment = true,
+							nilness = true,
+							unusedparams = true,
+							unusedwrite = true,
+							useany = true,
+						      },
+						      usePlaceholders = true,
+						      completeUnimported = true,
+						      staticcheck = true,
+						      directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+						      semanticTokens = true,
 					},
 				},
 				lua_ls = {
@@ -147,7 +171,27 @@ return {
 					},
 				},
 			},
-			setup = {},
+			setup = {
+			        gopls = function(_, opts)
+			          -- workaround for gopls not supporting semanticTokensProvider
+			          -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
+			          LazyVim.lsp.on_attach(function(client, _)
+			            if not client.server_capabilities.semanticTokensProvider then
+			              local semantic = client.config.capabilities.textDocument.semanticTokens
+			              client.server_capabilities.semanticTokensProvider = {
+			                full = true,
+			                legend = {
+			                  tokenTypes = semantic.tokenTypes,
+			                  tokenModifiers = semantic.tokenModifiers,
+			                },
+			                range = true,
+			              }
+			            end
+			          end, "gopls")
+			          -- end workaround
+			        end,
+		      },
+
 		},
 	},
 
